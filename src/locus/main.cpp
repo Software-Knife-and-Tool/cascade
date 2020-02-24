012e6ca527cab1f52a782aefbb8a2ab050e201ba
James M. Putnam <putnamjm.design@gmail.com>
2020-02-23 21:39:57 -0500

    issue #2 - locus framework

--- src/locus/main.cpp
+++ src/locus/main.cpp
@@ -1,14 +1,15 @@
+#include <QDesktopWidget>
 #include <QApplication>
 
 #include "MainWindow.h"
 
 int main(int argc, char **argv) {
-  
-  QApplication app(argc, argv);
 
-  locus::MainWindow mainWindow;
+  QApplication app(argc, argv);
   
+  locus::MainWindow mainWindow;
   mainWindow.show();
   
+  mainWindow.setFixedSize(1024, 768);
   return app.exec();
 }
