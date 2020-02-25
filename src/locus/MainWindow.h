9f10512d2db1e614f08aa72c48ca168c580366fe
James M. Putnam <putnamjm.design@gmail.com>
2020-02-25 17:31:00 -0500

    issue #2 - locus framework

--- src/locus/MainWindow.h
+++ src/locus/MainWindow.h
@@ -34,61 +34,10 @@ class MainWindow : public QMainWindow {
   }
 
  protected:
-  void contextMenuEvent(QContextMenuEvent *event) override;
-
- private slots:
-  void newFile();
-  void open();
-  void save();
-  void print();
-  void undo();
-  void redo();
-  void cut();
-  void copy();
-  void paste();
-  void bold();
-  void italic();
-  void leftAlign();
-  void rightAlign();
-  void justify();
-  void center();
-  void setLineSpacing();
-  void setParagraphSpacing();
-  void about();
-  void aboutQt();
+  // void contextMenuEvent(QContextMenuEvent *event) override;
 
  private:
-  void createActions();
-  void createMenus();
   void createStatusBar();
-
-  QMenu *fileMenu;
-  QMenu *editMenu;
-  QMenu *locusMenu;
-  QMenu *formatMenu;
-  QMenu *helpMenu;
-
-  QActionGroup *alignmentGroup;
-  QAction *newAct;
-  QAction *openAct;
-  QAction *saveAct;
-  QAction *printAct;
-  QAction *exitAct;
-  QAction *undoAct;
-  QAction *redoAct;
-  QAction *cutAct;
-  QAction *copyAct;
-  QAction *pasteAct;
-  QAction *boldAct;
-  QAction *italicAct;
-  QAction *leftAlignAct;
-  QAction *rightAlignAct;
-  QAction *justifyAct;
-  QAction *centerAct;
-  QAction *setLineSpacingAct;
-  QAction *setParagraphSpacingAct;
-  QAction *aboutAct;
-  QAction *aboutQtAct;
 
  private:
   QDateTime startTime;
