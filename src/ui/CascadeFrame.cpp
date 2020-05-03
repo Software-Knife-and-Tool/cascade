98ab09ab8da26a94eb4df6980602e335f2b66f36
James M. Putnam <putnamjm.design@gmail.com>
2020-05-02 20:15:22 -0400

    #37 - cascade

--- src/ui/CascadeFrame.cpp
+++ src/ui/CascadeFrame.cpp
@@ -38,6 +38,7 @@
  **/
 
 #include <QtWidgets>
+#include <QString>
 
 #include "CascadeFrame.h"
 
@@ -46,10 +47,36 @@ namespace cascade {
   CascadeFrame::CascadeFrame(QWidget*)
     : mu(new Mu()), consoleWidget(new ConsoleWidget(this, mu)) {
     
-  QString hostName = QSysInfo::machineHostName();
-  QString userName = qgetenv("USER");
+  auto hostName = QSysInfo::machineHostName();
+  auto userName = qgetenv("USER");
 
-  QString system_html =
+  std::string html =
+    "<html>"
+    "<body bgcolor=#c0c0c0>"
+    "<span style=\"text-align: center; font-family:Eaglefeather\">"
+    "<div>"
+    "<h1>Logica Cascade IDE <i>0.0.1</i></h1>"
+    "<p></p>"
+    "<h2>running on <i>blackloch.local</i>, an Intel Core I7 workstation</h2>"
+    "<p></p>"
+    "<p style=\"h3\">"
+    "<h3>"
+    "canon <i>0.0.1</i><br>"
+    "libmu <i>0.0.1</i><br>"
+    "mu <i>0.0.1</i>"
+    "</h3>"
+    "</p>"
+    "<p></p>"
+    "<h2>running on macOs Mojave 10.14.6</h2>"
+    "</div>"
+    "<p></p>"
+    "</span>"
+    "</body>"
+    "</html>";
+    
+  auto system_html = QString::fromStdString(html);
+
+#if 0
     QString("<div align=left><p><h1>Cascade ") +
     QString("0.0.1") +
     QString("</h1></p>") +
@@ -61,7 +88,8 @@ namespace cascade {
     QString("<h3>") + userName +
     QString(" running on ") + hostName +
     QString("</h3></div");
-    
+ #endif
+  
   bannerLabel = new QLabel(system_html);
   bannerLabel->setFrameStyle(QFrame::StyledPanel | QFrame::Sunken);
   bannerLabel->setAlignment(Qt::AlignCenter);
