90404e311f7e216ed9401166a29ffade2fd5685a
James M. Putnam <putnamjm.design@gmail.com>
2020-08-15 12:02:13 -0400

    #123 - add scripting frame

---
+++ src/ui/ScriptFrame.cpp
@@ -0,0 +1,181 @@
+/*******
+ **
+ ** Copyright (c) 2017, James M. Putnam
+ ** All rights reserved.
+ **
+ ** Redistribution and use in source and binary forms, with or without
+ ** modification, are permitted provided that the following conditions are met:
+ **
+ ** 1. Redistributions of source code must retain the above copyright notice,
+ **    this list of conditions and the following disclaimer.
+ **
+ ** 2. Redistributions in binary form must reproduce the above copyright
+ **    notice, this list of conditions and the following disclaimer in the
+ **    documentation and/or other materials provided with the distribution.
+ **
+ ** 3. Neither the name of the copyright holder nor the names of its
+ **    contributors may be used to endorse or promote products derived from
+ **    this software without specific prior written permission.
+ **
+ ** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ ** AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ ** IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ ** ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ ** LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ ** CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ ** SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ ** INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ ** CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ ** ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ ** POSSIBILITY OF SUCH DAMAGE.
+ **
+ *******/
+
+/********
+ **
+ **  ScriptFrame.cpp: ScriptFrame implementation
+ **
+ **/
+#include <QFileDialog>
+#include <QLabel>
+#include <QSplitter>
+#include <QTextEdit>
+#include <QToolBar>
+#include <QString>
+#include <QtWidgets>
+
+#include "ScriptFrame.h"
+#include "canon.h"
+
+namespace logicaide {
+
+void ScriptFrame::clear() {
+  editText->setText("");
+  evalText->setText("");
+}
+
+void ScriptFrame::load() {
+  loadFileName =
+    QFileDialog::getOpenFileName(this,
+                                 tr("Load File"),
+                                 tabBar->userInfo()->userdir(),
+                                 tr("File (*)"));
+
+  QFile f(loadFileName);
+  if (f.open(QFile::ReadOnly | QFile::Text)) {
+    QTextStream in(&f);
+    editText->setText(in.readAll());
+    f.close();
+  }
+
+  saveFileName = loadFileName;
+}
+    
+void ScriptFrame::eval() {
+  QString out;
+
+  tabBar->setContextStatus(tr("eval"));
+  
+  auto error =
+    canon->withException([this, &out]() {
+         out = canon->rep(editText->toPlainText());
+       });
+
+  evalText->setText(out + error);
+
+  emit evalHappened(editText->toPlainText());
+}
+
+void ScriptFrame::reset() {
+  canon = new Canon();
+}
+
+void ScriptFrame::del() {
+}
+
+void ScriptFrame::save_as() {
+  saveFileName = QFileDialog::getSaveFileName(this,
+        tr("Save As"), "",
+        tr("File (*)"));
+  save();
+}
+
+void ScriptFrame::save() {
+  QString text = editText->toPlainText();
+  
+  QSaveFile file(saveFileName);
+  file.open(QIODevice::WriteOnly);
+  file.write(text.toUtf8());
+  file.commit();
+}
+
+bool ScriptFrame::eventFilter(QObject *watched, QEvent *event) {
+  if ( /* watched == textEdit && */ event->type() == QEvent::KeyPress) {
+    QKeyEvent *e = static_cast < QKeyEvent * >(event);
+    if (e->key() == Qt::Key_Return &&
+        e->modifiers() & Qt::ShiftModifier) {
+      eval();
+      return true;
+    }
+  }
+    
+  return tabBar->get_mw()->eventFilter(watched, event);
+}
+  
+ScriptFrame::ScriptFrame(QString name, MainTabBar* tb, Canon* cn)
+  : tabBar(tb), canon(cn), name(name) {
+  
+  auto size = this->frameSize();
+
+  toolBar = new QToolBar();
+  connect(toolBar->addAction(tr("clear")),
+          &QAction::triggered, this, &ScriptFrame::clear);
+  connect(toolBar->addAction(tr("load")),
+          &QAction::triggered, this, &ScriptFrame::load);
+  connect(toolBar->addAction(tr("eval")),
+          &QAction::triggered, this, &ScriptFrame::eval);
+  connect(toolBar->addAction(tr("reset")),
+          &QAction::triggered, this, &ScriptFrame::reset);
+  connect(toolBar->addAction(tr("save")),
+          &QAction::triggered, this, &ScriptFrame::save);
+  connect(toolBar->addAction(tr("save as")),
+          &QAction::triggered, this, &ScriptFrame::save_as);
+  connect(toolBar->addAction(tr("del")),
+          &QAction::triggered, this, &ScriptFrame::del);
+
+  editText = new QTextEdit();
+  editScroll = new QScrollArea();
+  editScroll->setWidget(editText);
+  editScroll->setWidgetResizable(true);
+  editScroll->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+  editScroll->installEventFilter(this);
+
+  evalText = new QLabel();
+  evalScroll = new QScrollArea();
+  evalScroll->setWidget(evalText);
+  evalScroll->setWidgetResizable(true);
+  evalScroll->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+  evalScroll->installEventFilter(this);
+  evalScroll->setMinimumHeight(size.height() / 2);
+  
+  QSizePolicy spEdit(QSizePolicy::Preferred, QSizePolicy::Preferred);
+  spEdit.setVerticalStretch(1);
+  editText->setSizePolicy(spEdit);
+
+  QSizePolicy spEval(QSizePolicy::Preferred, QSizePolicy::Preferred);
+  spEval.setVerticalStretch(1);
+  evalText->setSizePolicy(spEval);
+
+  auto vs = new QSplitter(Qt::Vertical, this);
+  vs->addWidget(editScroll);
+  vs->addWidget(evalScroll);
+
+  auto layout = new QVBoxLayout;
+  layout->setContentsMargins(5, 5, 5, 5);
+  layout->addWidget(toolBar);
+  layout->addWidget(vs);
+  
+  setLayout(layout);
+}
+
+} /* logicaide namespace */
---
+++ src/ui/ScriptFrame.h
@@ -0,0 +1,114 @@
+/*******
+ **
+ ** Copyright (c) 2017, James M. Putnam
+ ** All rights reserved.
+ **
+ ** Redistribution and use in source and binary forms, with or without
+ ** modification, are permitted provided that the following conditions are met:
+ **
+ ** 1. Redistributions of source code must retain the above copyright notice,
+ **    this list of conditions and the following disclaimer.
+ **
+ ** 2. Redistributions in binary form must reproduce the above copyright
+ **    notice, this list of conditions and the following disclaimer in the
+ **    documentation and/or other materials provided with the distribution.
+ **
+ ** 3. Neither the name of the copyright holder nor the names of its
+ **    contributors may be used to endorse or promote products derived from
+ **    this software without specific prior written permission.
+ **
+ ** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ ** AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ ** IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ ** ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ ** LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ ** CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ ** SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ ** INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ ** CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ ** ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ ** POSSIBILITY OF SUCH DAMAGE.
+ **
+ *******/
+
+/********
+ **
+ **  ScriptFrame.h: ScriptFrame class
+ **
+ **/
+#ifndef _LOGICAIDE_SRC_UI_SCRIPTFRAME_H_
+#define _LOGICAIDE_SRC_UI_SCRIPTFRAME_H_
+
+#include <QFrame>
+#include <QLabel>
+#include <QScrollArea>
+#include <QTextEdit>
+#include <QToolBar>
+#include <QWidget>
+
+#include "canon.h"
+#include "MainTabBar.h"
+
+QT_BEGIN_NAMESPACE
+class QLabel;
+class QScroll;
+class QTextEdit;
+class QToolBar;
+class QVBoxLayout;
+class QWidget;
+QT_END_NAMESPACE
+
+namespace logicaide {
+
+class MainTabBar;
+class MainWindow;
+  
+class ScriptFrame : public QFrame {
+
+ Q_OBJECT
+
+ public:
+  explicit ScriptFrame(QString, MainTabBar*, Canon*);
+
+  signals:
+    void evalHappened(QString);
+
+ private:
+  void clear();
+  void eval();
+  void load();
+  void reset();
+  void save();
+  void save_as();
+  void del();
+  
+  void setContextStatus(QString str) {
+    tabBar->setContextStatus(str);
+  }
+
+  void showEvent(QShowEvent* event) override {
+    QWidget::showEvent(event);
+    tabBar->setContextStatus(name);
+  }
+  
+  bool eventFilter(QObject, QEvent*);
+    void log(QString msg) { tabBar->log(msg); }
+
+  bool eventFilter(QObject*, QEvent*) override;
+
+  QString loadFileName;
+  QString saveFileName;
+
+  MainTabBar *tabBar;
+  Canon* canon;
+  QString name;
+  QTextEdit* editText;
+  QLabel* evalText;
+  QToolBar* toolBar;
+  QScrollArea* editScroll;
+  QScrollArea* evalScroll;
+};
+
+} /* logicaide namespace */
+
+#endif  /* _LOGICAIDE_SRC_UI_SCRIPTFRAME_H_ */
