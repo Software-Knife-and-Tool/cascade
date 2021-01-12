c4281f11d5b9154ca9b17a086f78b4e83a27e9e0
James M. Putnam <putnamjm.design@gmail.com>
2021-01-11 19:16:49 -0500

    #156 - menu operations classes

---
+++ src/ui/FrameMenu.cpp
@@ -0,0 +1,29 @@
+/********
+ **
+ **  SPDX-License-Identifier: BSD-3-Clause
+ **
+ **  Copyright (c) 2017-2021 James M. Putnam <putnamjm.design@gmail.com>
+ **
+ **/
+
+/********
+ **
+ **  FrameMenu.cpp: FrameMenu class
+ **
+ **/
+#include <QtWidgets>
+
+#include "FrameMenu.h"
+#include "MainMenuBar.h"
+#include "MainWindow.h"
+
+namespace gyreui {
+  
+void FrameMenu::envFrame() {
+  mw->setContextStatus("<b>Frame|Env</b>");
+}
+
+FrameMenu::FrameMenu(MainMenuBar* mb)
+    : mb(mb), mw(mb->mw_()) {}
+
+} /* namespace gyreui */
---
+++ src/ui/FrameMenu.h
@@ -0,0 +1,51 @@
+/********
+ **
+ **  SPDX-License-Identifier: BSD-3-Clause
+ **
+ **  Copyright (c) 2017-2021 James M. Putnam <putnamjm.design@gmail.com>
+ **
+ **/
+
+/********
+ **
+ **  FrameMenu.h: FrameMenu class
+ **
+ **/
+#ifndef GYREUI_UI_FRAMEMENU_H_
+#define GYREUI_UI_FRAMEMENU_H_
+
+#include <QMainWindow>
+#include <QMenu>
+
+#include "MainMenuBar.h"
+#include "MainWindow.h"
+
+QT_BEGIN_NAMESPACE
+class QAction;
+class QActionGroup;
+class QLabel;
+class QMenu;
+QT_END_NAMESPACE
+
+namespace gyreui {
+
+class MainMenuBar;
+class MainWindow;
+
+class FrameMenu : public QMenu {
+  Q_OBJECT
+
+ public:
+  void envFrame();
+
+  
+  explicit FrameMenu(MainMenuBar*);
+
+ private:
+  MainMenuBar* mb;
+  MainWindow* mw;
+};
+
+} /* namespace gyreui */
+
+#endif /* GYREUI_UI_FRAMEMENU_H_ */
